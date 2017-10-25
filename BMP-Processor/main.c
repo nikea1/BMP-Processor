83930af55f0753b64c04134356a3f9184e7db6c2
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2017-10-25 05:11:30 -0400

    Initial Commit

---
+++ BMP-Processor/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BMP-Processor
+//
+//  Created by Adenike Adeyemi on 10/25/17.
+//  Copyright © 2017 Adenike Adeyemi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
