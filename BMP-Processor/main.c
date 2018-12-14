02dc4a7fd5b3ec5636193f9498cddb9dbcc783b8
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2018-12-13 22:54:50 -0500

    display cube without map matrix

--- BMP-Processor/main.c
+++ BMP-Processor/main.c
@@ -16,30 +16,30 @@ BMP bmp;
 
 int main(int argc, const char * argv[]) {
     
-    bmp = parseBMPFile("alpha.bmp");
-    unsigned char* test = bmpBGRA(bmp);
+    bmp = parseBMPFile("test2.bmp");
+   // unsigned char* test = bmpBGRA(bmp);
     
     printf("The signiture of the BMP file is '%c%c'.\n", bmp.bitMapFileHeader.signiture[0], bmp.bitMapFileHeader.signiture[1]);
     printf("The imagesize of the BMP file is %d bytes. \n", bmp.bitMapFileHeader.fileSize);
-    printf("The imageOffset of the BMP file is %x. \n",bmp.bitMapFileHeader.imgOffset);
+    printf("The imageOffset of the BMP file is %x. \n",bmp.bitMapFileHeader.imgOffset); //for opengl check for this
     
     printf("Header Size: %d bytes\n", bmp.imageInfoHeader.V3.headerSize);
-    printf("Header Height: %d bytes\n", bmp.imageInfoHeader.V3.height);
-    printf("Header Width: %d bytes\n", bmp.imageInfoHeader.V3.width);
+    printf("Header Height: %d bytes\n", bmp.imageInfoHeader.V3.height);//for opengl check for this
+    printf("Header Width: %d bytes\n", bmp.imageInfoHeader.V3.width);//for opengl check for this
     printf("bpp: %d\n", bmp.imageInfoHeader.V1.bpp);
-    printf("Image map size: %d\n", bmp.imageInfoHeader.V1.imageSize);
+    printf("Image map size: %d\n", bmp.imageInfoHeader.V1.imageSize); //for open gl check for this
  
     
-    printf("Alpha mask: %d\n", bmp.imageInfoHeader.V3.alphaMask);
+    printf("Alpha mask: %d\n", bmp.imageInfoHeader.V3.alphaMask); //for opengl check for this
     printf("Red mask: %d\n", bmp.imageInfoHeader.V3.redMask);
     printf("Blue mask: %d\n", bmp.imageInfoHeader.V3.blueMask);
     printf("Green mask: %d\n", bmp.imageInfoHeader.V3.greenMask);
-    
+    /*
     for(int i = 0; i < bmp.imageInfoHeader.V1.imageSize; i++){
         if(i%16 == 0) printf("\n");
         printf("%x ", test[i]);
         
-    }
+    }*/
     
     printf("\n");
     printf("\n");
@@ -53,7 +53,7 @@ int main(int argc, const char * argv[]) {
     
 
     decontructBMPPixelArray(bmp);
-    free(test);
+    //free(test);
     
     return 0;
 }
